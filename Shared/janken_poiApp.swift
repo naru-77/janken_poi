5350032b8b959f02f1984e38b842627f3a329afd
naru-77 <naru.fumi@icloud.com>
2021-09-27 17:33:12 +0900

    Initial Commit

---
+++ Shared/janken_poiApp.swift
@@ -0,0 +1,17 @@
+//
+//  janken_poiApp.swift
+//  Shared
+//
+//  Created by ナルセ　ユウキ on 2021/09/27.
+//
+
+import SwiftUI
+
+@main
+struct janken_poiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
